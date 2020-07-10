55d7e1f1b1cbea8da326d5b3b01103a888f6a153
肌肉仔 <itzhengwei163.com>
2020-07-10 20:00:48 +0800

    登录页布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app">
     <router-view></router-view>
   </div>
 </template>
@@ -10,6 +10,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less'>
+body,html,.app{
+  width: 100%;
+  height: 100%;
 
+}
 </style>
